1bad3b03c8f9a334fe0200ffec713f0f730e4ee1
soilihi ambdouroihmane <soilihiambdouroihmane@gmail.com>
2024-07-03 09:37:47 +0000

    update

---
+++ main.js
@@ -0,0 +1,12 @@
+
+// Déclarer une variable de type int et la convertir en String puis l'afficher par alerte.
+let maVariable = 12;
+
+maVariable = "Fallou"
+
+alert("maVariable = "+maVariable);
+
+// Déclarer une variable de type date affiche la date en prenant en compte la date d'aujourd'hui (jj-mm-yy)
+let date = new Date();
+
+console.log("Date : "+date.getDay()+" / "+date.getMonth()+" / "+date.getFullYear());
